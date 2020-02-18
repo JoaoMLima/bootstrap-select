def93f7a84d18f8c4a9d5e5416e6ce75496d9fd7
JoaoMLima <joao.medeiros@ccc.ufcg.edu.br>
2020-02-18 16:19:06 -0300

    Solved portuguese translation

--- js/i18n/defaults-pt_BR.js
+++ js/i18n/defaults-pt_BR.js
@@ -7,6 +7,7 @@
 (function ($) {
   $.fn.selectpicker.defaults = {
     noneSelectedText: 'Nada selecionado',
+    addItemText: 'Adicionar {0} às opções selecionadas',
     noneResultsText: 'Nada encontrado contendo {0}',
     countSelectedText: 'Selecionado {0} de {1}',
     maxOptionsText: ['Limite excedido (máx. {n} {var})', 'Limite do grupo excedido (máx. {n} {var})', ['itens', 'item']],
